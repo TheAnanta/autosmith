bc7e420a1342127eba934a30305277b0fc7298d3
Manas Malla <manasmalla.dev@gmail.com>
2023-09-15 03:33:23 +0530

    added authentication platforms like phone auth, email auth with password and bloc listeners
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
